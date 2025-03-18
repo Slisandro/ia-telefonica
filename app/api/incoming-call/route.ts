3485f18db2028f849b66ac2c7ca93db45a419670
Lisandro <salvareschilisandro0@gmail.com>
2025-03-18 17:34:08 +0000

    fix bug

--- app/api/incoming-call/route.ts
+++ app/api/incoming-call/route.ts
@@ -4,7 +4,7 @@ import Twilio from "twilio";
 export async function POST() {
   const client = Twilio(
     "ACf3e1333cfb9fd46bbae472ab05967fc6",
-    "23613fa872622446bf948b09aecc9365"
+    process.env.TWILIO_AUTH_TOKEN
   );
 
   try {

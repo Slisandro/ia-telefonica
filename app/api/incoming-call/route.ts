00dfa088c2626abb2186c08e63a62a0f082781c5
Lisandro <salvareschilisandro0@gmail.com>
2025-03-18 17:51:26 +0000

    fix bug

--- app/api/incoming-call/route.ts
+++ app/api/incoming-call/route.ts
@@ -3,14 +3,14 @@ import Twilio from "twilio";
 
 export async function POST() {
   const client = Twilio(
-    "ACf3e1333cfb9fd46bbae472ab05967fc6",
-    "23613fa872622446bf948b09aecc9365"
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
   );
 
   try {
     const call = await client.calls.create({
       url: "https://handler.twilio.com/twiml/EH01a1d48e4ea9fdcad67416a404c5f25f",
-      to: "+542235396722", // Número destino
+      to: "+17873787307", // Número destino
       from: "+16318142082", // Tu número de Twilio
     });
 
